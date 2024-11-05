4480fbc7e91ff0d9a2f4318979f0d22136e4e2a3
Apollo <apollomacabodbod8@gmail.com>
2024-11-05 14:54:35 +0800

    added template literals

--- app/_components/home-cube.tsx
+++ app/_components/home-cube.tsx
@@ -8,10 +8,10 @@ export default function HomeCube() {
 
         <div className="relative">
 
-          <p className="text-[3.5em]  md:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.05em]  md:tracking-[0.15em] font-serif    leading-[0.9em] transition-all duration-1000 ease-in-out">
+          <p className="text-[3.5em]  md:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.05em]  md:tracking-[0.12em] font-serif    leading-[0.9em] transition-all duration-1000 ease-in-out">
                     Welcome 
             <br className="lg:hidden transition-all duration-1000 ease-in-out"/> 
-                    to our 
+            {`${" to our"}`}
           </p>
 
           <p className="text-[3.5em]  md:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.02em] font-serif  leading-[1.4em] md:leading-[1.4em] transition-all duration-1000 ease-in-out">
@@ -25,7 +25,7 @@ export default function HomeCube() {
             width={176}
             height={176}
             // className="border border-[#50B498] absolute top-[2em] left-[16em] md:left-[22em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight"
-            className="border border-[#50B498] absolute top-[3.5em] lg:top-[2em] left-[14.5em]  md:left-[19em] lg:left-[25em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight transition-all duration-1000 ease-in-out"
+            className="border border-[#50B498] absolute top-[3.5em] lg:top-[2em] left-[14.5em]  md:left-[18em] lg:left-[25em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight transition-all duration-1000 ease-in-out"
           />
 
 
