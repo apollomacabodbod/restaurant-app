656ae512dddc21f307d7aeae382878bd0d4f8d47
Apollo <apollomacabodbod8@gmail.com>
2024-11-05 14:25:35 +0800

    fix absolute values

--- app/_components/home-cube.tsx
+++ app/_components/home-cube.tsx
@@ -8,13 +8,13 @@ export default function HomeCube() {
 
         <div className="relative">
 
-          <p className="text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.06em] font-serif  leading-[1em]">
+          <p className="text-[3.5em]  md:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.05em]  md:tracking-[0.15em] font-serif    leading-[0.9em]">
                     Welcome 
             <br className="lg:hidden"/> 
                     to our 
           </p>
 
-          <p className="text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.02em] font-serif  leading-[1em]">
+          <p className="text-[3.5em]  md:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.02em] font-serif  leading-[1.4em] md:leading-[1.4em]">
                     Restaurant
           </p>
 
@@ -22,9 +22,10 @@ export default function HomeCube() {
           <Image
             src={'/cube.svg'}
             alt="facebook logo"
-            width={141}
+            width={176}
             height={176}
-            className="border border-[#50B498] absolute top-[2em] left-[22em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight"
+            // className="border border-[#50B498] absolute top-[2em] left-[16em] md:left-[22em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight"
+            className="border border-[#50B498] absolute top-[3.5em] lg:top-[2em] left-[14.5em]  md:left-[19em] lg:left-[25em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight"
           />
 
 
