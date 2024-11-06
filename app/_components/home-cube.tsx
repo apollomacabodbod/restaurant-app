5bbc7ec47de27966376f329e91054908073736c9
Apollo <apollomacabodbod8@gmail.com>
2024-11-06 14:42:14 +0800

    fix: responsive in cube icon

--- app/_components/home-cube.tsx
+++ app/_components/home-cube.tsx
@@ -6,21 +6,21 @@ export default function HomeCube() {
       <div className="flex flex-col border border-[#50B498] mt-[2.6875em] mx-[1em] lg:mx-[8.4375em] sm:mt-[9.625em] transition-all duration-1000 ease-in-out">
               
 
-        <div className="sm:flex sm:items-center transition-all duration-1000 ease-in-out">
+        <div className="sm:flex sm:items-center sm:justify-between transition-all duration-1000 ease-in-out">
 
 
           <div className="flex flex-col  ">
 
             <div className="relative">
 
-              <p className="text-[3.5em]  sm:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.05em]  sm:tracking-[0.12em] font-serif    leading-[0.9em] transition-all duration-1000 ease-in-out">
-Welcome 
-                <br className="lg:hidden transition-all duration-1000 ease-in-out"/> 
+              <p className="text-[3.5em]  sm:text-[3.2rem]  md:text-[4rem]  lg:text-[4rem] xl:text-[4.5rem]  whitespace-nowrap border border-[#50B498] font-bold  font-serif tracking-[0.09em]  lg:tracking-[0.09em] transition-all duration-1000 ease-in-out">
+               Welcome 
+                <br className="sm:hidden transition-all duration-1000 ease-in-out"/> 
                 {`${" to our"}`}
               </p>
 
-              <p className="text-[3.5em]  sm:text-[4.5rem] border border-[#50B498] font-bold  tracking-[0.02em] font-serif  leading-[1.4em] sm:leading-[1.4em] transition-all duration-1000 ease-in-out">
-Restaurant
+              <p className="text-[3.5em] sm:text-[3.2rem]  md:text-[4rem]  lg:text-[4rem] xl:text-[4.5rem]  border border-[#50B498] font-bold  tracking-[0.02em] font-serif  leading-[1.4em] sm:leading-[1.4em] transition-all duration-1000 ease-in-out">
+               Restaurant
               </p>
 
               <div className="flex items-center">
@@ -32,7 +32,7 @@ Restaurant
                   width={176}
                   height={176}
                   // className="border border-[#50B498] absolute top-[2em] left-[16em] md:left-[22em] -translate-x-1/2 -translate-y-1/2 leading-tight tracking-tight"
-                  className="border border-[#50B498] absolute top-[3.5em] lg:top-[2em] left-[14.5em]  md:left-[18em] lg:left-[25em] -translate-x-1/2 -translate-y-1/2  transition-all duration-1000 ease-in-out"
+                  className="border border-[#50B498] absolute top-[3.5em] sm:top-[1.7em]    left-[14.5em]   sm:left-[13.5em] md:left-[16em]  lg:left-[16em] xl:left-[24em]  -translate-x-1/2 -translate-y-1/2  transition-all duration-1000 ease-in-out"
                 />
 
 
