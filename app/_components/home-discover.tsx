c977affd8069d44cfd34675fe5da005d922ef64b
apollo <apollomacabodbod8@gmail.com>
2024-11-09 15:34:57 +0800

    added container flex

--- app/_components/home-discover.tsx
+++ app/_components/home-discover.tsx
@@ -8,25 +8,45 @@ export default function HomeDiscover(){
     
     <>
 
-      <div className="flex flex-col my-[5em] lg:my-[10em] border border-[#50B498] green">
+      <div className="flex flex-col my-[5em] lg:my-[10em] border border-[#50B498] green transition-all duration-1000 ease-in-out">
 
-        <div className="relative ">
-          <Image
-            src={'/home-kitchen.svg'}
-            alt="oval logo"
-            width={570}
-            height={560}
-            className=" min-w-[3em]"
-          />
-          <Image
-            src={'/path.svg'}
-            alt="path logo"
-            width={15}
-            height={16}
-            className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          />
+
+
+        <div className="sm:flex sm:items-center sm:justify-between">
+
+          <div className="relative flex flex-col ">
+            <Image
+              src={'/home-kitchen.svg'}
+              alt="oval logo"
+              width={570}
+              height={560}
+              className=" min-w-[3em]"
+            />
+            <Image
+              src={'/path.svg'}
+              alt="path logo"
+              width={15}
+              height={16}
+              className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+
+
+          <div className="flex flex-col">
+
+            <p>Discover</p>
+
+          </div>
+
+
+
+
         </div>
+      
 
+
+
+        
 
 
       </div>
