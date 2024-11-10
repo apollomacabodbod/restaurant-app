825811167c71cc6cb260583bcfd2ec527b01790d
apollo <apollomacabodbod8@gmail.com>
2024-11-10 14:23:24 +0800

    added list of items

--- app/_components/home-checkout.tsx
+++ app/_components/home-checkout.tsx
@@ -28,58 +28,61 @@ export default function HomeCheckout(){
 
         <div className="flex items-center justify-between border border-[#50B498] green">
 
-          <p className="border border-[#50B498] green">Alaskan Fish & Chips</p>
+          <p className="border border-[#50B498] green text-[#333] font-roboto text-[1.25rem] not-italic font-light">Alaskan Fish & Chips</p>
 
-          <p className="border border-[#50B498] green">$14.00</p>
+          <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[1.25rem] not-italic font-light ">$14.00</p>
 
         </div>
 
 
-        <p className="border border-[#50B498] green">Served with soy sauce, wasabi, pickled ginger and chopsticks</p>
+        <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[0.875rem] not-italic font-normal ">Served with soy sauce, wasabi, pickled ginger and chopsticks</p>
      
      
 
 
         <div className="flex items-center justify-between border border-[#50B498] green">
 
-          <p className="border border-[#50B498] green">Fishermen’s Basket</p>
+          <p className="border border-[#50B498] green text-[#333] font-roboto text-[1.25rem] not-italic font-light">Fishermen’s Basket</p>
 
-          <p className="border border-[#50B498] green">$7.00</p>
+          <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[1.25rem] not-italic font-light ">$7.00</p>
 
         </div>
 
 
-        <p className="border border-[#50B498] green">Snow crab, avocado, cucumber, tobiko, Japanese mayonnaise</p>
- 
+        <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[0.875rem] not-italic font-normal ">Snow crab, avocado, cucumber, tobiko, Japanese mayonnaise</p>
+     
 
 
 
 
         <div className="flex items-center justify-between border border-[#50B498] green">
 
-          <p className="border border-[#50B498] green">Chicken Breat Saute Sec</p>
+          <p className="border border-[#50B498] green text-[#333] font-roboto text-[1.25rem] not-italic font-light">Chicken Breat Saute Sec</p>
 
-          <p className="border border-[#50B498] green">$5.00</p>
+          <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[1.25rem] not-italic font-light ">$5.00</p>
 
         </div>
 
 
-        <p className="border border-[#50B498] green">A Latte is a coffee drink made with espresso and steamed milk</p>
- 
+        <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[0.875rem] not-italic font-normal ">A Latte is a coffee drink made with espresso and steamed milk</p>
+     
 
 
 
         <div className="flex items-center justify-between border border-[#50B498] green">
 
-          <p className="border border-[#50B498] green">Crisp Pork Belly</p>
+          <p className="border border-[#50B498] green text-[#333] font-roboto text-[1.25rem] not-italic font-light">Crisp Pork Belly</p>
 
-          <p className="border border-[#50B498] green">$18.00</p>
+          <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[1.25rem] not-italic font-light ">$18.00</p>
 
         </div>
 
 
-        <p className="border border-[#50B498] green">Candied salmon, basil potato puree, baby kale marsala</p>
- 
+        <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[0.875rem] not-italic font-normal ">Candied salmon, basil potato puree, baby kale marsala</p>
+
+
+
+
 
         <div className="flex items-center justify-center">
     
