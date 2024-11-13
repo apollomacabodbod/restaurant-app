c696165be061f13cdac7a34d21df592edd861615
apollo <apollomacabodbod8@gmail.com>
2024-11-14 07:07:00 +0800

    added home-coffe-night component

---
+++ app/_components/home-coffee-night.tsx
@@ -0,0 +1,22 @@
+
+
+
+export default function HomeCoffeNight(){
+
+  return (
+    
+    <>
+    
+      <div className="flex flex-col">
+
+        <div className="flex items-center justify-center"></div>
+
+
+      </div>
+
+
+    
+    </>
+    
+  )
+}
