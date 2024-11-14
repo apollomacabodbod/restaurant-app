02980d5cac0a54f86be1746d05bcf2041b118db3
apollo <apollomacabodbod8@gmail.com>
2024-11-14 14:25:43 +0800

    remove borders in carousel

--- app/_components/home-upcoming-events.tsx
+++ app/_components/home-upcoming-events.tsx
@@ -7,11 +7,11 @@ export default function HomeUpcomingEvents(){
   return (<>
 
 
-    <div className="flex flex-col border border-[#50B498] green mt-[6.5em] mx-[2em] lg:mx-[8.4375em]">
+    <div className="flex flex-col  mt-[6.5em] mx-[2em] lg:mx-[8.4375em]">
 
       <div className="flex items-center justify-center">
 
-        <p className="border border-[#50B498] green text-[#FFF] font-roboto text-[1.5rem] not-italic font-light">Party with us!</p>
+        <p className=" text-[#FFF] font-roboto text-[1.5rem] not-italic font-light">Party with us!</p>
 
 
       </div>
@@ -19,7 +19,7 @@ export default function HomeUpcomingEvents(){
 
       <div className="flex items-center justify-center">
 
-        <p className="border border-[#50B498] green text-[#FFF] font-pt-serif  text-[2rem] lg:text-[3.5rem] not-italic font-bold ">Upcoming Events</p>
+        <p className=" text-[#FFF] font-pt-serif  text-[2rem] lg:text-[3.5rem] not-italic font-bold ">Upcoming Events</p>
 
       </div>
 
