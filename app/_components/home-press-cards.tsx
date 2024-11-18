b01156ee2eeacf7757a6e88ba82f3d68fa431f77
apollo <apollomacabodbod8@gmail.com>
2024-11-18 11:07:58 +0800

    adjust padding in small screens

--- app/_components/home-press-cards.tsx
+++ app/_components/home-press-cards.tsx
@@ -8,9 +8,9 @@ export default function HomePressCards(){
   return (<>
 
 
-    <div className="grid grid-cols-1 xl:grid-cols-3  border border-[#50B498] green mx-[2em] lg:mx-[8.4375em] gap-[1.875em] my-[4.3125em] ">
+    <div className="grid grid-cols-1 xl:grid-cols-3  border border-[#50B498] green mx-[2em] lg:mx-[8.1em] gap-[1.875em] my-[4.3125em] ">
 
-      <div className="flex flex-col border border-[#50B498] green  py-[3.0625em] px-[1.3em] bg-white">
+      <div className="flex flex-col border border-[#50B498] green  py-[3.0625em] px-[1em] bg-white">
 
         <div className="flex items-center justify-center border border-[#50B498] green">
 
@@ -18,13 +18,13 @@ export default function HomePressCards(){
         </div>
 
         <p className="border border-[#50B498] green text-center text-[#333] font-roboto text-[1.25rem] not-italic font-light xl:max-w-[17.125em] mt-[1.9em]">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level.</p>
-        <p className="border border-[#50B498] green text-center text-[#333] font-pt-serif text-[1.2rem] lg:text-[1.5rem] not-italic font-bold mt-[1.875em]">The New York Times</p>
+        <p className="border border-[#50B498] green text-center text-[#333] font-pt-serif text-[1.5rem] not-italic font-bold mt-[1.875em]">The New York Times</p>
 
       </div>
 
 
 
-      <div className="flex flex-col border border-[#50B498] green  py-[3.0625em] px-[1.3em] bg-white ">
+      <div className="flex flex-col border border-[#50B498] green  py-[3.0625em] px-[1em] bg-white ">
 
         <div className="flex items-center justify-center border border-[#50B498] green">
 
@@ -32,13 +32,13 @@ export default function HomePressCards(){
         </div>
 
         <p className="border border-[#50B498] green text-center text-[#333] font-roboto text-[1.25rem] not-italic font-light xl:max-w-[17.125em] mt-[1.5em]">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level.</p>
-        <p className="border border-[#50B498] green text-center text-[#333] font-pt-serif text-[1.2rem] lg:text-[1.5rem] not-italic font-bold mt-[1.875em]">The Guardian</p>
+        <p className="border border-[#50B498] green text-center text-[#333] font-pt-serif text-[1.5rem] not-italic font-bold mt-[1.875em]">The Guardian</p>
 
       </div>
 
 
 
-      <div className="flex flex-col border border-[#50B498] green pt-[3.0625em] px-[1.3em] bg-white">
+      <div className="flex flex-col border border-[#50B498] green pt-[3.0625em] px-[1em] bg-white">
 
         <div className="flex items-center justify-center border border-[#50B498] green">
 
@@ -46,7 +46,7 @@ export default function HomePressCards(){
         </div>
 
         <p className="border border-[#50B498] green text-center text-[#333] font-roboto text-[1.25rem] not-italic font-light xl:max-w-[17.125em] mt-[2.5625em]">Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level.</p>
-        <p className="border border-[#50B498] green text-center text-[#333] font-pt-serif text-[1.2rem] lg:text-[1.5rem] not-italic font-bold mt-[1.875em]">The Wall Street Journal</p>
+        <p className="border border-[#50B498] green text-center text-[#333] font-pt-serif text-[1.5rem] not-italic font-bold mt-[1.875em]">The Wall Street Journal</p>
 
       </div>
 
