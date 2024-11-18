9ceb2b40a8728e1a15804aaa79d51d22c3a5d863
apollo <apollomacabodbod8@gmail.com>
2024-11-18 17:12:06 +0800

    added home-our-chef-1 icon

--- app/_components/home-our-chef.tsx
+++ app/_components/home-our-chef.tsx
@@ -10,21 +10,53 @@ export default function HomeOurChef(){
   return (<>
 
 
-    <div className="flex flex-col border border-[#50B498] green mt-[10em] mx-[2em] lg:mx-[8.4375em]">
+    <div className="flex flex-col border border-[#50B498] green my-[10em] mx-[2em] lg:mx-[5em]  xl:mx-[8.4375em]">
 
 
-      <div className="lg:flex lg:items-center lg:justify-between lg:gap-[8.125em] lg:border border-[#50B498] green">
+      <div className="lg:flex lg:items-center lg:justify-between lg:gap-[2em] xl:gap-[8.125em] lg:border border-[#50B498] green">
 
 
 
 
         <div className="flex flex-col border border-[#50B498] green">
 
-          <div className="flex items-center justify-center lg:justify-start border border-[#50B498] green">
+          <div className="flex items-center justify-center lg:justify-start border border-[#50B498] green relative">
 
             <Image src={'/home-our-chef.svg'} width={570} height={560} alt="our chef icon"></Image>
 
+          
+            <div className="absolute top-[12em]  lg:top-[18em] xl:top-[20.8125em] left-[2em]  sm:left-[0em] sm:hidden block lg:block  -translate-x-1/2 -translate-y-1/2    transition-all duration-1000 ease-in-out object-cover">
+          
+              <div className="relative ">
+
+
+                <div className="flex flex-col bg-[#BBA285] w-[120px] h-[120px] rounded-[0.5em] "></div>
+
+
+                <Image
+                  src={'/kitchen-oval.svg'}
+                  alt="oval logo"
+                  width={85}
+                  height={85}
+                  className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                />
+                <Image
+                  src={'/home-our-chef-1.svg'}
+                  alt="path logo"
+                  width={33}
+                  height={48}
+                  className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                />
+
+
+              </div>
+      
+            </div>
+          
           </div>
+
+
+       
 
 
         </div>
@@ -37,7 +69,7 @@ export default function HomeOurChef(){
 
           <p className="border border-[#50B498] green lg:max-w-[29.375em] text-[#333] font-roboto text-[1.25rem] not-italic font-light ">A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred music, or a noble picture, or a passage from the grander poets. It always does one good.</p>
                 
-          <p className="border border-[#50B498] green text-[#333] font-pt-serif text-[1.5rem] font-bold mt-[2.875em]"> Slavcho Karbashewski</p>
+          <p className="border border-[#50B498] green text-[#333] font-pt-serif text-[1.5rem] font-bold mt-[2em]"> Slavcho Karbashewski</p>
 
           <p className="border border-[#50B498] green text-[rgba(51,51,51,0.50)] font-roboto text-[1.25rem] not-italic font-light" >Head Chef</p>
                 
