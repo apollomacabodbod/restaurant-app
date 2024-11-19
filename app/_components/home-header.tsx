6fe64f5b4e3b5d2afdefad2aa5ae77185873d325
apollo <apollomacabodbod8@gmail.com>
2024-11-20 07:05:29 +0800

    remove comments in header component

--- app/_components/home-header.tsx
+++ app/_components/home-header.tsx
@@ -3,8 +3,6 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 import Sidebar from "./home-sidebar-mobile";
 import { useHeaderState } from "@/hooks/useHeader";
 
