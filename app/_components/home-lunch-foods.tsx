56c6b59860e080ec375ad206c40eb26f1ced24e3
apollo <apollomacabodbod8@gmail.com>
2024-11-21 07:53:04 +0800

    remove borders

--- app/_components/home-lunch-foods.tsx
+++ app/_components/home-lunch-foods.tsx
@@ -30,13 +30,13 @@ export default function HomeLunchFoods(){
   return (<>
 
 
-    <div className="grid grid-cols-1 lg:grid-cols-2 mt-[1em] mx-[2em] lg:mx-[3em] xl:mx-[8.4375em]  transition-all duration-1000 ease-in-out  gap-[1em] lg:gap-[1em] border border-[#50B498] green"
+    <div className="grid grid-cols-1 lg:grid-cols-2 mt-[1em] mx-[2em] lg:mx-[3em] xl:mx-[8.4375em]  transition-all duration-1000 ease-in-out  gap-[1em] lg:gap-[1em] "
     
     >
 
       {homeFoods.map(item => (
 
-        <motion.div key={item.id} className="flex flex-col  transition-all duration-1000 ease-in-out border border-[#50B498] green"
+        <motion.div key={item.id} className="flex flex-col  transition-all duration-1000 ease-in-out "
         
         
           initial="hidden"
@@ -51,7 +51,7 @@ export default function HomeLunchFoods(){
 
         >
 
-          <div className="lg:flex lg:items-center lg:justify-between lg:gap-[1.25em] border border-[#50B498] green">
+          <div className="lg:flex lg:items-center lg:justify-between lg:gap-[1.25em] ">
 
 
             <div className="flex flex-col "> 
@@ -71,7 +71,7 @@ export default function HomeLunchFoods(){
 
 
 
-            <div className="flex flex-col border border-[#50B498] green">
+            <div className="flex flex-col ">
 
               <div className="flex items-center justify-between   gap-[7em]">
                    
